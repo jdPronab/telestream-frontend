67e9a765000d08c03cb7eb065497f3c89234a1d6
jdPronab <jdpronab@gmail.com>
2025-08-26 19:31:39 +0530

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import './App.css'
 import Dashboard from './pages/Dashboard'
+import Files from './pages/Files'
+import Earnings from './pages/Earnings'
+import Reports from './pages/Reports'
+import Settings from './pages/Settings'
 
 function App() {
 
@@ -10,6 +14,10 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/files' element={<Files />} />
+      <Route path='/earnings' element={<Earnings />} />
+      <Route path='/daily/reports' element={<Reports />} />
+      <Route path='/settings' element={<Settings />} />
     </Routes>
   )
 }
